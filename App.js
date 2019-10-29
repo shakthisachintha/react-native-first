eab7f00347cd4e06bacdf70a512b65d2e8ec9ef7
shakthisachintha <43114582+shakthisachintha@users.noreply.github.com>
2019-10-29 21:17:24 +0530

    development

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Button,ImageBackground,Vibration,ToastAndroid,TextInput,StyleSheet,Text, View } from 'react-native';
+
+
+const styles=StyleSheet.create({
+  center:{
+    textAlign:'center'
+  },
+  big:{
+    fontWeight:'bold',
+    fontSize:30,
+  },
+  blue:{
+    color:'blue',
+  },
+  red:{
+    color:'red'
+  }
+});
 
 class Greeting extends Component {
   render() {
     return (
       <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
+        <Text style={this.props.style} >Hello {this.props.name}!</Text>
       </View>
     );
   }
 }
 
+
 export default class LotsOfGreetings extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={};
+  }
+
   render() {
     return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-      </View>
+      <ImageBackground source={{uri:'https://www.elsetge.cat/myimg/f/29-295735_android-wallpaper-gallery-minimalist-wallpaper-for-android.jpg'}} style={{width: '100%', height: '100%'}}>
+       
+          <TextInput style={{height:50}} placeholder="Enter Your Name" onChangeText={(val) => this.setState({val})} />
+          <Greeting style={[styles.big,styles.red]} name={this.state.val} />
+          <Button style={{width:20,height:20}} onPress={() => {ToastAndroid.show('You Touched The Button', 200);Vibration.vibrate(100);}} title="Press Me"/>
+         
+      </ImageBackground>
     );
   }
 }
